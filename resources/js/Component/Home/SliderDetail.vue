43811da16a8400c950d43de761fae9ca0a00f84e
lokman-hosen <lokman@mcc.com.bd>
2023-11-06 17:47:57 +0600

    Home page breaks into small component

---
+++ resources/js/Component/Home/SliderDetail.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="carousel-item active">
+        <img class="w-100" src="/assets/frontend/img/carousel-1.jpg" alt="Image">
+        <div class="carousel-caption">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-lg-10">
+                        <h5 class="text-light text-uppercase mb-3 animated slideInDown">Welcome to NDE Real Estate Ltd.</h5>
+                        <h1 class="display-2 text-light mb-3 animated slideInDown">A Construction & Renovation Company</h1>
+                        <ol class="breadcrumb mb-4 pb-2">
+                            <li class="breadcrumb-item fs-5 text-light">Commercial</li>
+                            <li class="breadcrumb-item fs-5 text-light">Residential</li>
+                            <li class="breadcrumb-item fs-5 text-light">Industrial</li>
+                        </ol>
+                        <!--                                    <a href="" class="btn btn-primary py-3 px-5">More Details</a>-->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
